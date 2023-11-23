35d9e6a7e1adf21500566dc35235a3e2d642a96c
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-11-24 00:02:35 +0600

    Installed cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+import mongoose from 'mongoose'
+import config from './app/config'
+import app from './app'
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.database_url}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
