d099cc7545f2db02505fc35af32a13505e91099b
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-11-24 14:12:29 +0600

    Created the model

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,26 +1,32 @@
-import { Schema } from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
 import { TUser } from './user.interface'
 
-const userSchema = new Schema<TUser>({
-  userId: { type: Number, unique: true },
-  username: { type: String, unique: true },
-  password: { type: String },
-  fullName: {
-    firstName: { type: String },
-    lastName: { type: String },
-  },
-  age: { type: Number },
-  email: { type: String },
-  isActive: { type: Boolean },
-  hobbies: [{ type: String }],
-  address: {
-    street: { type: String },
-    city: { type: String },
-    country: { type: String },
-  },
-  orders: [
-    { productName: { type: String } },
-    { price: { type: Number } },
-    { quantity: { type: Number } },
-  ],
+const orderSchema = new Schema({
+  productName: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
 })
+
+const userSchema = new Schema<TUser>({
+  userId: { type: Number, unique: true, required: true },
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, required: true },
+  hobbies: [{ type: String, required: true }],
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  orders: [orderSchema],
+})
+
+const UserModel = mongoose.model<TUser>('User', userSchema)
+
+export default UserModel
